88a1253a05d0fe846dbcc85c4fcd26efeb10392b
James P <james.piaggio13@gmail.com>
2020-04-15 19:39:00 -0400

    Added addPhraseToDisplay method to Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -5,7 +5,20 @@ class Phrase {
     constructor (phrase) {
         this.phrase = phrase.toString().toLowerCase();
     }
+    /* Display Phrase on screen */
     addPhraseToDisplay() {
-        
+        const phraseList = document.querySelector('ul');
+        for(let i = 0; i < this.phrase.length; i++) {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = this.phrase[i];
+            phraseList.appendChild(listItem);
+            if(listItem.innerHTML === ' ') {
+                listItem.classList.add('space');
+            } else {
+                listItem.classList.add('hide');
+                listItem.classList.add('letter');
+                listItem.classList.add(listItem.innerHTML);
+            }
+        }
     }
 }
